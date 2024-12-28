67ce5ca0f2d8df0dffd806d7ca6ec0bce18b0ccd
Hesam Fallahian <mal.fallahian@gmail.com>
2024-12-28 14:06:13 -0500

    variables

---
+++ variables.tf
@@ -0,0 +1,44 @@
+variable "project_id" {
+    type= string
+    default = "terraform-446100"
+}
+
+variable "region" {
+    type= string
+    default = "us-west1"
+}
+
+variable "zone" {
+    type= string
+    default = "us-west1-a"
+}
+
+variable "network_name" {
+    type= string
+    default = "app"
+}
+
+variable "network_ip_range" {
+    type= string
+    default = "10.2.0.0/16"
+}
+
+variable "image_project" {
+    type= string
+    default = "ubuntu-os-cloud"
+}
+
+variable "image_family" {
+    type= string
+    default = "ubuntu-2204-lts"
+}
+
+variable "app_name" {
+    type= string
+    default = "web"
+}
+
+variable "machine_type" {
+    type= string
+    default = "e2-micro"
+}
