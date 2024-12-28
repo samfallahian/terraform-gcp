1d106e2fb0e8b82321632902cd6748163c6195d3
Hesam Fallahian <mal.fallahian@gmail.com>
2024-12-28 11:27:40 -0500

    network updated

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "google_compute_network" "app" {
 resource "google_compute_subnetwork" "app" {
   name          = "app"
   ip_cidr_range = "10.2.0.0/16"
-  region        = "us-west1"
+  region        = "us-west1-a"
   network       = google_compute_network.app.id
 }
 
